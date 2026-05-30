8c31e2c8b26a877451df056b2adb2a487fd54fbf
Charles Nkonoki <charlitokenn@gmail.com>
2026-05-30 23:09:01 +0300

    Initial commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,3 @@
+export default async function AuthLayout({ children }: { children: React.ReactNode }) {
+    return <main className="flex items-center justify-center h-screen">{children}</main>
+}
---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const AppConfig ={
+    name: "Rhema ERP",
+    description: "Internal finance workflow",
+    version: "1.0.0",
+    logo: "/next.svg"
+}
